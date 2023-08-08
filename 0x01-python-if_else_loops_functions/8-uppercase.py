047c21d5f478f7a953c2e6069b4e055ddec02acd
mainanorbert <mainanorbert@gmail.com>
2023-08-08 20:33:06 +0000

    converts letters to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
-        if 97 <= ord(letter) <= 122:
-            upper_l = ord(letter) - 32
-            print("{}".format(chr(upper_l)), end="")
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            upper_l = chr(ord(letter) - 32)
+            print("{}".format(upper_l), end="")
         else:
             print("{}".format(letter), end="")
     print()
