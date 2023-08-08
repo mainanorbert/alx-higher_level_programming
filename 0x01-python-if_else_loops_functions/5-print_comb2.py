48e02dd6a75bcfb805e6632e9992dc7c8e30eae2
mainanorbert <mainanorbert@gmail.com>
2023-08-08 14:40:26 +0000

    prints numbers 0 - 99 with spaces and comma

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 for x in range(0, 100):
-    if x < 10:
-        print("0{0}".format(x), end="")
+    if x == 99:
+        print("{}".format(x))
     else:
-        print("{0}".format(x), end="")
-    if x != 99:
-        print(", ", end="")
-print("")
+        print("{:02}".format(x), end=", ")
