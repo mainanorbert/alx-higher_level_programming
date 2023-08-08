98af2539dec8b1c4a5b2b7161bea68a973b00967
mainanorbert <mainanorbert@gmail.com>
2023-08-08 15:08:46 +0000

    prints all possible combinations of two digit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for x in range(10):
+    for y in range(x+1, 10):
+        if x == 8 and y == 9:
+            print("{0}{1}".format(x, y))
+        else:
+            print("{0}{1}".format(x, y), end=", ")
