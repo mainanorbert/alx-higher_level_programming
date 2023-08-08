3d1643b32e638f0bb31a627ab42d77e1ec77f89f
mainanorbert <mainanorbert@gmail.com>
2023-08-08 20:22:36 +0000

    converst words to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for letter in str:
+        if 97 <= ord(letter) <= 122:
+            upper_l = ord(letter) - 32
+            print("{}".format(chr(upper_l)), end="")
+        else:
+            print("{}".format(letter), end=""   )
+    print();
