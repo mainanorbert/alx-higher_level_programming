05d13b4368149a6b9686b65328f5854056e2618d
mainanorbert <mainanorbert@gmail.com>
2023-08-10 18:12:30 +0000

    prints names of compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    s = "name"
-    print(s[:2])
     strs = dir(hidden_4)
-
     for str in strs:
         if str[:2] != "__":
             print(str)
