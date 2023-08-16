402897e91bd0050134e34b663b79baf700881052
mainanorbert <mainanorbert@gmail.com>
2023-08-16 19:39:48 +0000

    multiplies values of dictionary by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = {}
     if isinstance(a_dictionary, dict):
         for k, v in a_dictionary.items():
-            a_dictionary[k] = v*2
-        return a_dictionary
+            new_dict[k] = v*2
+        return new_dict
