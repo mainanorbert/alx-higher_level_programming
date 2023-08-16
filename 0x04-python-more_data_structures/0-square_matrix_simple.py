878035e5ffea20a52a9daca775750c54202db59a
mainanorbert <mainanorbert@gmail.com>
2023-08-16 10:25:52 +0000

    function that cumputes squares of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        sq_matrix = []
-        for row in matrix:
-            sq_row = []
-            for x in row:
-                sq_row.append(x**2)
-            sq_matrix.append(sq_row)
-        return sq_matrix
+    sq_matrix = []
+    for row in matrix:
+        sq_row = []
+        for x in row:
+            sq_row.append(x**2)
+        sq_matrix.append(sq_row)
+    return sq_matrix
