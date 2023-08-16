868e084e80e73518d5a58093f988bf1c8910e19b
mainanorbert <mainanorbert@gmail.com>
2023-08-16 18:21:12 +0000

    functions that searches and replaces

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for x in my_list:
-        if x == search:
-            new_list.append(replace)
-        else:
-            new_list.append(x)
+    new_list = my_list[:]
+    for x in range(len(my_list)):
+        if new_list[x] == search:
+            new_list[x] = replace
     return new_list
