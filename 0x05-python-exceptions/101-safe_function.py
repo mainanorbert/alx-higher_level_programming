41ce8f29dc44dc6fb148f1b6ee833de0b3386ddf
mainanorbert <mainanorbert@gmail.com>
2023-08-28 19:12:46 +0000

    a function executing a function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,11 +2,9 @@
 def safe_function(fct, *args):
     import sys
 
-
     try:
         func_result = fct(*args)
         return func_result
     except Exception as e:
         print("Exception: ", e, file=sys.stderr)
         return None
-
