fe0e2894faf223c6754891a509d090ffab8c5355
mainanorbert <mainanorbert@gmail.com>
2023-08-29 12:09:58 +0000

    A square class with private attribute

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Defining square class'''
+
+
+class Square:
+    '''representing a square class'''
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("the size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
