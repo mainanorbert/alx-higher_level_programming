443ecf75c9e833bc4d2c7eb32d55acfb87b7cf14
mainanorbert <mainanorbert@gmail.com>
2023-08-29 12:23:51 +0000

    A square class with private attribute

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,6 +14,6 @@ class Square:
         size (int): size of the square'''
         if not isinstance(size, int):
             raise TypeError("the size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
