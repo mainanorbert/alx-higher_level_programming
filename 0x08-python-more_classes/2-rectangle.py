f7261bd3e3b624387b99f32371e66655ab9150b9
mainanorbert <mainanorbert@gmail.com>
2023-09-04 10:34:13 +0000

    finding tha area and perimeter of Class Rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Function that defines a rectangle"""
+
+
+class Rectangle:
+    """
+    Defining a class
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get or set width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get or set the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns area of rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """returning perimeter of a rectangle"""
+        return ((2 * self.width) + (2 * self.height))
