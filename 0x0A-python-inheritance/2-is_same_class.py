d1859d8ae895b2d1b148e92a0e6008e4e1cbc7f8
mainanorbert <mainanorbert@gmail.com>
2023-09-11 14:40:26 +0000

    function that checks type of object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,7 +9,8 @@ def is_same_class(obj, a_class):
     Args:
         obj (any type): the objects to be checked
         a_class (type of object): type against which to check
-    Return: return True otherwise False
+    Returns:
+        return True otherwise False
     """
     if isinstance(obj, a_class):
         return True
