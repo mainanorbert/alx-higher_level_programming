2c52b850eb7ef72420d72b37b68fcf44c43099de
mainanorbert <mainanorbert@gmail.com>
2023-09-13 20:01:42 +0000

    adding new attrubute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,15 @@
 
 
 def add_attribute(my_obje, attr_name, att_value):
+    """function adding new attribute to object
+
+    Args:
+        my_obje(any): object of any type
+        attr_name: name of new attribute
+        att_value: value to add
+    Raises:
+        TypeError: if attribute exists
+    """
     if not hasattr(my_obje, attr_name):
         setattr(my_obje, attr_name, att_value)
     else:
