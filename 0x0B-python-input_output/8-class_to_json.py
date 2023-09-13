340facbab93af23a3bdd850b5230584433d8118c
mainanorbert <mainanorbert@gmail.com>
2023-09-13 21:03:34 +0000

    return json of dictionary description

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,4 +4,9 @@
 
 class class_to_json(obj):
     """function returning dict representation of simple data structure"""
-    return obj.__dict__
+    attr_dict = {}
+    for atr in dir(obj):
+        atr_v = getattr(obj, atr)
+        if isinstance(atr_v, (list, dict, str, int, bool)):
+            atrr_dict[atr] = atr_v
+    return atrr_dict
