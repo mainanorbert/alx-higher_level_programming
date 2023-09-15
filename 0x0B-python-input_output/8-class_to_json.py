84fdb5c63a198b00ba2bb808eb35eb562808bd23
mainanorbert <mainanorbert@gmail.com>
2023-09-15 10:00:00 +0000

    converting class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -10,9 +10,4 @@ class class_to_json(obj):
     Returns:
         returns dictionary of all methods and objects
     """
-    attr_dict = {}
-    for atr in dir(obj):
-        atr_v = getattr(obj, atr)
-        if isinstance(atr_v, (list, dict, str, int, bool)):
-            atrr_dict[atr] = atr_v
-    return atrr_dict
+    return obj.__dict__
