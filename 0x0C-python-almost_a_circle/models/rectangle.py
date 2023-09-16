c903774b391f08761f48f93eb7e6b1974ec11596
mainanorbert <mainanorbert@gmail.com>
2023-09-16 07:26:05 +0000

    validating attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,32 +23,52 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getting and setting rect width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
+        """getting and setting rect width"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
+        """getting and setting x coordinate for the table"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >=   0")
         self.__x = value
 
     @property
     def y(self):
+        """getting and setting y coordinate for the table"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.__y = y
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
