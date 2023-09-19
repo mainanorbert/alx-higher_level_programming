c4516edf5b159ad1151b4df0fa5e9085eca59dc2
mainanorbert <mainanorbert@gmail.com>
2023-09-19 04:59:18 +0000

    documenting rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from models.base import Base
 """Rectangle class defined"""
+from models.base import Base
 
 
 class Rectangle(Base):
-    """defining a class for rectangle"""
+    """representing a  rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializing rectangle
