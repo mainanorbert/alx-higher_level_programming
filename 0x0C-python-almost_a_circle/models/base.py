ccd75771b67442de40593ac8a68c74b746d96fa7
mainanorbert <mainanorbert@gmail.com>
2023-09-19 16:40:59 +0000

    None to come first

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,7 +30,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returning json representation of dictionary"""
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
