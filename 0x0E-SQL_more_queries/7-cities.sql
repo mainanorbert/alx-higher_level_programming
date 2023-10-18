aeb44bf0b24827ead1c1980d79f8de6f1e1c9ff4
mainanorbert <mainanorbert@gmail.com>
2023-10-18 17:22:16 +0000

    git push

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- creating cities table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	PRIMARY KEY(id),
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES states(id)
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 	);
