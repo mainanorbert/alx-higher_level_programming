b1b2b84bbbc869da4e63896ffb90f54fcaf566c1
mainanorbert <mainanorbert@gmail.com>
2023-10-18 13:30:14 +0000

    creating a table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- creating table call force table
-CREATE TABLE IF NOT EXISTS force_table(id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
