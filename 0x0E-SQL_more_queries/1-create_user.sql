df99409b6ecf88e7b1240b796b5a5bf28522b9d1
mainanorbert <mainanorbert@gmail.com>
2023-10-18 11:55:29 +0000

    creating user with all prvileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creating mysql server user user_0d_1
 -- granting all privileges and setting password
-CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
 FLUSH PRIVILEGES;
