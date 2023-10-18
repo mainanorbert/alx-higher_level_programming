a6bd50adbf5b7dcd1e59c200ea78328b1591fdbe
mainanorbert <mainanorbert@gmail.com>
2023-10-18 17:15:05 +0000

    states table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+CREATE TABLE IF NOT EXISTS cities(
 	PRIMARY KEY(id),
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+	FOREIGN KEY(state_id) REFERENCES states(id)
 	);
