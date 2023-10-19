3162ff218f4bfb8e6f0378fb9341e245396fc2bf
mainanorbert <mainanorbert@gmail.com>
2023-10-19 06:02:30 +0000

    ordering score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
- -- displaus records with score more than= 10 in DESC
-SELECT score, name
-FROM second_table
-WHERE score>=10
-ORDER BY score DESC;
+-- displaus records with score more than= 10 in DESC
+SELECT score, name FROM second_table WHERE score>=10 ORDER BY score DESC;
