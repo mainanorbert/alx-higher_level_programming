3c3971a9c1087565d1f5723016c7843c8be1e146
mainanorbert <mainanorbert@gmail.com>
2023-10-19 09:43:16 +0000

    joing tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Listing cities in database
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states on cities.state_id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
