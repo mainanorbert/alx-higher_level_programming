76d0101d0799370e8b53bb674f3e578e7b12bba2
mainanorbert <mainanorbert@gmail.com>
2023-10-19 10:40:20 +0000

    listing all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- listing all shows in database
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genre ON tv_show.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id
