ddb897569779eaad7f09256f55cf4dce1f07fae2
mainanorbert <mainanorbert@gmail.com>
2023-11-13 20:56:53 +0000

    printing square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const size = Math.floor(Number(process.argv[2]));
-if (isNaN(size)){
+if (isNaN(size)) {
 	console.log('Size Missing');
 } else {
 	for (let x = 0; x < size; x++) {
 		let row = '';
-		for (c = 0; c < size; c++){
+		for (c = 0; c < size; c++) {
 			row += 'X';
 		}
 		console.log(row);
