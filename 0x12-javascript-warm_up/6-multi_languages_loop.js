392faa6ad9bea3f5c29e98e860299746c8d54032
mainanorbert <mainanorbert@gmail.com>
2023-11-13 20:23:32 +0000

    multiple line

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 const my_language = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let x = 0; x < my_language.length; x++)
-	console.log(my_language[x]);
+for (let x of my_language)
+{
+	console.log(x);
+}
