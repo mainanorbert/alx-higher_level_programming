a093326659a6505a5d556e26f1ece9e8d003c06d
mainanorbert <mainanorbert@gmail.com>
2023-11-14 19:36:43 +0000

    importing and computing array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const mylist = require('./100-data.js').list;
 console.log(mylist);
-const newlist = mylist.map((val, idx) => val * idx)
-console.log(newlist)
+const newlist = mylist.map((val, idx) => val * idx);
+console.log(newlist);
