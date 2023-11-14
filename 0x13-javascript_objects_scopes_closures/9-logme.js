a2d912cb5adf7198dc8db3e679e690683e29e7af
mainanorbert <mainanorbert@gmail.com>
2023-11-14 17:41:25 +0000

    printing no of args

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.logMe = function (item) {
+  if (exports.logMe.count === undefined) {
+    exports.logMe.count = 0;
+  }
+    console.log(`${exports.logMe.count}: ${item}`);
+    exports.logMe.count++;
+}
