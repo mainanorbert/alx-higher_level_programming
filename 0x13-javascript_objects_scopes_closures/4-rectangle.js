78f8ff552f5a4dd64030e1c3fcb220de9a8906e1
mainanorbert <mainanorbert@gmail.com>
2023-11-15 08:29:24 +0000

    printing rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -5,21 +5,24 @@ module.exports = class Rectangle {
       this.width = w;
       this.height = h;
     }
-    }
-      print () {
-       for (let i = 0; i < this.height; i++) {
-         let rect = '';
-	 for (let x = 0; x < this.width; x++) {
-	   rect += "X";
-	 }
-	   console.log(rect);
-       }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let x = 0; x < this.width; x++) {
+        rect += 'X';
       }
-        rotate () {
-	  [this.width, this.height] = [this.height, this.width];
-	}
-	double () {
-	  this.width = this.width * 2;
-	  this.height = this.height * 2;
-	}
-  };
+      console.log(rect);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
