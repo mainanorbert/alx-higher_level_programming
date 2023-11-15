235b10c70e91283971f5efd0705a95d64500e3ab
mainanorbert <mainanorbert@gmail.com>
2023-11-15 08:25:48 +0000

    semi std

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,14 +5,15 @@ module.exports = class Rectangle {
       this.width = w;
       this.height = h;
     }
-    }
-      print () {
-       for (let i = 0; i < this.height; i++) {
-         let rect = '';
-	 for (let x = 0; x < this.width; x++) {
-	   rect += "X";
-	 }
-	   console.log(rect);
-       }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let x = 0; x < this.width; x++) {
+        rect += 'X';
       }
-  };
+      console.log(rect);
+    }
+  }
+};
