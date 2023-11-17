032562fdf24b7be65b13f512ebf4aa91e31f90b8
mainanorbert <mainanorbert@gmail.com>
2023-11-17 10:39:05 +0000

    displays matching name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          password=argv[2], db=argv[3])
     db_obj = db.cursor()
-    db_obj.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}' ORDER BY id ASC".format(argv[4]))
+    db_obj.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}' \
+            ORDER BY id ASC".format(argv[4]))
     for state in db_obj.fetchall():
-            print(state)
+        print(state)
     db_obj.close()
     db.close()
