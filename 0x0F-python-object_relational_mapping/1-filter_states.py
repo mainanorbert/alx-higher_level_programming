b5554e5e431f4dff445d3602f7e8462643aece3d
mainanorbert <mainanorbert@gmail.com>
2023-11-17 09:30:12 +0000

    selectings states starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Listing states with name starting with N"""
+from sys import argv
+import MySQLdb
+
+if "__name__" == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], db=argv[3])
+    db_obj = db.cursor()
+    db_obj.execute("SELECT * FROM `states` ORDER BY `id`")
+    for state in db_obj.fetchall():
+        if (state[1][0] == "N"):
+            print(state)
+
+    db.close()
+    db_obj.close()
