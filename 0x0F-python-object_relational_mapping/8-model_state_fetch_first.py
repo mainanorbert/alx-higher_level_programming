42fb895fbb99c74493a073e07ef2d0f82b7367b0
mainanorbert <mainanorbert@gmail.com>
2023-11-19 18:58:19 +0000

    lisitng first obj

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Prints first state object from db
+"""
+from sys import arg
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ = "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(arg[1], arg[1], arg[2]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    s = session.query(State).order_by(State.id).first()
+    if s is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(s.id, s.name))
