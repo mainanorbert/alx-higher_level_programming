d696b6994618ef815a74d73d4253f956ad335fa2
mainanorbert <mainanorbert@gmail.com>
2023-11-20 06:56:48 +0000

    state objects with a letter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,12 +9,11 @@ from sqlalchemy.orm import sessionmake
 
 if __name__ == "__main__":
     db_engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-
     Session = sessionmaker(bind=db_engine)
     session = Session()
     Base.metadata.create_all()
-    states = session.query(State).filter(State.name.like('%a\
-            %')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like('%a%'))\
+            .order_by(State.id).all()
     for s in states:
         print("{}: {}".format(s.id, s.name))
     session.close()
