0d4450abb16dc70aef99774eb7031e97c38c24b7
Norberts <mainanorbert@gmail.comm>
2023-11-30 16:52:08 +0300

    bash script for post reques

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # email set to test@gmail.com
 # subject set to 'I will always be here for PLD'
-curl -sx POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
