0f04ffb0f878a411f8e9d9967d2d59fdb0dcd28d
Norberts <mainanorbert@gmail.comm>
2023-12-01 20:18:29 +0300

    fetching from url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,8 @@
 """Fetching https://intranet.hbtn.io/status."""
 import requests
 
-
 if __name__ == "__main__":
-    req = requests.get("https://intranet.hbtn.io/status").text
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(req)))
-    print("\t- content: {}".format(req))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
