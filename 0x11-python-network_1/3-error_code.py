8ba1e3ac3136bfab3da9201ca170b417ccfd7331
Norberts <mainanorbert@gmail.comm>
2023-12-01 20:32:33 +0300

    script that takes url

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,9 +10,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    myurl = sys.argv[1]
 
-    request = urllib.request.Request(url)
+    request = urllib.request.Request(myurl)
     try:
         with urllib.request.urlopen(request) as response:
             print(response.read().decode("ascii"))
