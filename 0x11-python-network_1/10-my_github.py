0071b1fb3d1c2482f892c7de44997cefd3b2db18
Norberts <mainanorbert@gmail.comm>
2023-12-01 19:34:32 +0300

    github api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
 """
 Python script that takes GitHub credentials
 (username and personal access token)
@@ -8,10 +6,12 @@ and uses the GitHub API to display your user ID.
 usage:
     ./10-my_github.py <user name> <personal access token>
 """
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
 
 
 if __name__ == "__main__":
-    # auth = requests.auth.HTTPBasicAuth(argv[1], argv[2]).json()
-    res = requests.get("https://api.github.com/user",
-                       auth=(argv[1], argv[2])).json()
-    print(res.get("id"))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
