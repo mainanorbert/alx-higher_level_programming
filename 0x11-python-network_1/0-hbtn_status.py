133ed08b63d364058737addbf8efacb7e8cfc198
Norberts <mainanorbert@gmail.comm>
2023-12-01 11:50:19 +0300

    using urllib to fetch website

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""  Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == "__main__":
+    myurl = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(myurl) as response:
+        content = response.read().decode('utf-8')
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content}")
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""  Python script that fetches https://alx-intranet.hbtn.io/status"""
+def nober(name=''):
+	print(name)
+nober("osiemo")
