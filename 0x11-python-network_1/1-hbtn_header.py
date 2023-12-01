cf6fd4a58d6899c6adab95deffb54e0c6d3da3e0
Norberts <mainanorbert@gmail.comm>
2023-12-01 12:20:41 +0300

    pycodestyle compliant

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends 
-a request to the URL and displays the value of the X-Request-Id variable found in the header
+"""Python script that takes in a URL, sends
+a request to the URL and displays the value of the X-Request-Id
+variable found in the header
 of the response."""
 import urllib.request
 import sys
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -12,4 +14,3 @@ if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         x_req_id = response.headers["X-Request-Id"]
         print(x_req_id)
-
