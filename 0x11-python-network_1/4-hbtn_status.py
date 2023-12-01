6e8159af13f085ecfd4ef34aac09a0b0285a867f
Norberts <mainanorbert@gmail.comm>
2023-12-01 19:47:51 +0300

    python script fetching status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     req = requests.get("https://intranet.hbtn.io/status").text
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- type: {}".format(type(req)))
+    print("\t- content: {}".format(req))
