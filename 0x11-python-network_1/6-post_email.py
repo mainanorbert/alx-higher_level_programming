3782e508734e1e3d479d7d3466cc985431c106f1
Norberts <mainanorbert@gmail.comm>
2023-12-01 19:41:34 +0300

    post req with email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,7 +5,7 @@ parameter, and finally displays the body of the response.
 Usage: ./6-post_email.py <URL> <email>
   - Displays the body of the response."""
 import sys
-import request
+import requests
 
 
 if __name__ == "__main__":
