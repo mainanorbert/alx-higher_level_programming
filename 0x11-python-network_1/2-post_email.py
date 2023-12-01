c2965cc9eaa8b9693bca8b044046fbd5c235ba5e
Norberts <mainanorbert@gmail.comm>
2023-12-01 20:39:59 +0300

    taking url as email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-module that fetches header id
+script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
 """
 from urllib.request import urlopen
 from sys import argv
 
 
 if __name__ == "__main__":
-    data = f"email={argv[2]}".encode('utf-8')
-    with urlopen(argv[1], data=data) as response:
+    my_data = f"email={argv[2]}".encode('utf-8')
+    with urlopen(argv[1], my_data=data) as response:
         content = response.read().decode('utf-8')
         print(content)
