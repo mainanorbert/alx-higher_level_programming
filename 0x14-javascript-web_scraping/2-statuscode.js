00651c209797b41d613bbdbb171964ad84d58e92
Norberts <mainanorbert@gmail.comm>
2024-01-03 16:26:37 +0300

    displaying status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 
 const request = require('request');
 const url = process.argv[2];
-request(url, (error, response) => {
-  if (error) {
-    console.error(error);
+request(url, (err, response) => {
+  if (err) {
+    console.error(err);
   } else {
-    console.log(response.statusCode);
+    console.log("Code:",response.statusCode);
   }
 });
