274f6b7b3399352dcc1b4e825a757e968e68f5f3
Norberts <mainanorbert@gmail.comm>
2024-01-04 06:01:18 +0300

    ff

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,17 +3,17 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, (err, response, body) => {
-	if (err) {
-		console.error(err);
-	}else {
-		const toDoData = JSON.parse(body);
-		const completedtask = {};
-		toDoData.forEach((todo) => {
-			if (todo.completed) {
-				const id = todo.userId;
-				completedtask[id] = (completedtask[id] || 0) + 1;
-			}
-		});
-		console.log(completedtask);
-	}
+  if (err) {
+    console.error(err);
+  } else {
+    const toDoData = JSON.parse(body);
+    const completedtask = {};
+    toDoData.forEach((todo) => {
+      if (todo.completed) {
+        const id = todo.userId;
+        completedtask[id] = (completedtask[id] || 0) + 1;
+      }
+    });
+    console.log(completedtask);
+  }
 });
