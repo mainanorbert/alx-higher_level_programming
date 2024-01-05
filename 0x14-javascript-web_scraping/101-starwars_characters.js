1596b5384a3f5a1832e35a1d4d30e01c061d11eb
Norberts <mainanorbert@gmail.comm>
2024-01-05 05:27:14 +0300

    printing characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,3 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
+const id = process.argv[2];
+const baseUrl = 'https://swapi-api.alx-tools.com/api/';
+request(baseUrl + 'films/' + id, (err, res, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const characters = JSON.parse(data).characters;
+    if (characters.length === 0) {
+      console.log('no characters');
+    }
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (err, res, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const charData = JSON.parse(data);
+          console.log(charData.name);
+        }
+      });
+    });
+  }
+});
