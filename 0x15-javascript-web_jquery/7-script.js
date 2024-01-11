8ca5ca2ae8063ece4b01ac8aafb450d299348a67
Norberts <mainanorbert@gmail.comm>
2024-01-11 07:00:17 +0300

    fetches the character name from URL

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,5 @@
-$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
-  $('DIV#character').text(data.name);
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+    $('DIV#character').text(data.name);
+  });
 });
